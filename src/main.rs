e5cef78a895853edbf56e5bd56531ff931f77bea
Prateek Kumar <me@prateek.page>
2026-07-15 23:02:44 +0400

    basic setup of axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#![forbid(unsafe_code)]
+#![warn(clippy::pedantic, clippy::nursery)]
+#![deny(clippy::unwrap_used, clippy::expect_used, clippy::panic)]
+
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app).await?;
+    Ok(())
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
